8073dc99c9f7888fb42a619c47ce291a4bed1d67
Michal Ozaniak <ozaniak.michal@gmail.com>
2019-03-14 20:41:25 +0100

    NFC tag detection

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -5,9 +5,42 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+
+  private var tableId: String? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    val intent = intent
+    val action = intent.action
+    val data = intent.data
+
+    if (NfcAdapter.ACTION_NDEF_DISCOVERED == action && data != null) {
+      handleNDEFDiscoveredIntent(intent)
+    }
+
+    MethodChannel(flutterView, "app.channel.shared.tableId").setMethodCallHandler { call, result ->
+      if (call.method.contentEquals("getTableId")) {
+        result.success(tableId)
+        tableId = null
+      }
+    }
+  }
+
+  internal fun handleNDEFDiscoveredIntent(intent: Intent) {
+    val data = intent.data
+    val pathSegments = data?.pathSegments
+    if (pathSegments?.getOrNull(0) == "table") {
+      tableId = pathSegments[1].toString()
+    }
   }
 }
